53ffb6abb61c1f701b9fc8c9d0e45c4b4e2ff931
Joseph2006t <laizatellojoseph@gmail.com>
2024-11-21 13:54:16 -0500

    segundo commit

--- Ejemplo1.js
+++ Ejemplo1.js
@@ -1,1 +1,3 @@
 console.log("Repositorio de prueba");
+console.log("Segundo commit para el repositorio de github");
+console.log("Mis lenguajes favoritos de progrmación son: c++, Java, Python, JavaScript");
